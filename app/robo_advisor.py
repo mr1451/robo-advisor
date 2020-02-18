70a215e393b94927b58bb0d7dc4997d7fbb70de3
mr1451 <mr1451@georgetown.edu>
2020-02-18 17:55:07 -0500

    Getting credentials from .env file.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,34 @@
 # app/robo_advisor.py
 
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("REQUESTING SOME DATA FROM THE INTERNET...")
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
+
+SYMBOL = "TSLA" #todo: ask for consumer input
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={SYMBOL}&apikey={API_KEY}"
+
+print("URL:", request_url)
+response = requests.get(request_url)
+
+print(type(response))
+print(response.status_code)
+print(type(response.text)) #> str
+
+parsed_response = json.loads(response.text)
+
+print(type(parsed_response)) #> dict
+
+print(parsed_response)
+
+#breakpoint()
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
