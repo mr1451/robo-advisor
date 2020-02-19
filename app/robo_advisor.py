74ac7f9a591e46ac6b7207d297e28c2fc3e3bdf7
mr1451 <mr1451@georgetown.edu>
2020-02-19 15:32:20 -0500

    Get closing price.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,9 +7,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#
-# INFO INPUTS 
-# 
+# utility function to convert float or integer to usd-formatted string
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
+# INFO INPUTS  
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
@@ -19,13 +21,15 @@ SYMBOL = "TSLA" #todo: ask for consumer input
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={SYMBOL}&apikey={API_KEY}"
 
 response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(response.text)
+#print(type(response))
+#print(response.status_code)
+#print(response.text)
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2020-02-19"]["4. close"]
 
 #print("URL:", request_url)
 #print(type(response.text)) #> str
@@ -44,6 +48,8 @@ last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
 #tsd = parsed_response["Time Series (Daily)"]
 
+# INFO OUTPUTS
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -51,7 +57,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
