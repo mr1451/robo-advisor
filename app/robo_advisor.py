208eeab62217007ab2cbb4770a19c99733ad01b5
mr1451 <mr1451@georgetown.edu>
2020-02-19 17:21:26 -0500

    Get latest high price!

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -37,6 +37,16 @@ latest_day = dates[0] #"2020-02-19"
 
 latest_close = tsd[latest_day]["4. close"]
 
+#get high price from each day
+high_prices = []
+
+for date in dates: 
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float(high_price))
+
+#maximum of all high prices
+recent_high = max(high_prices)
+
 #print("URL:", request_url)
 #print(type(response.text)) #> str
 
@@ -64,7 +74,7 @@ print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
