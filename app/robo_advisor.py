62d9f449e84088f938765fed3942c566a13e0d9a
mr1451 <mr1451@georgetown.edu>
2020-04-14 22:22:20 -0400

    Ensure program runs with initial functions.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,145 +1,127 @@
 # app/robo_advisor.py
 
 import csv
+import datetime
 import json
 import os
-import requests
-#from sendgrid import SendGridAPIClient
-#from sendgrid.helpers.mail import Mail
 
 from dotenv import load_dotenv
+import requests
 
-load_dotenv()
+load_dotenv() #> loads contents of the .env file into the script's environment
 
-# utility function to convert float or integer to usd-formatted string
+API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY", "demo") # default to using the "demo" key if an Env Var is not supplied
+
+def get_response(symbol):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    return parsed_response
+
+def transform_response(parsed_response):
+    # parsed_response should be a dictionary representing the original JSON response
+    # it should have keys: "Meta Data" and "Time Series Daily"
+    tsd = parsed_response["Time Series (Daily)"]
+
+    rows = []
+    for date, daily_prices in tsd.items(): # see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/dictionaries.md
+        row = {
+            "timestamp": date,
+            "open": float(daily_prices["1. open"]),
+            "high": float(daily_prices["2. high"]),
+            "low": float(daily_prices["3. low"]),
+            "close": float(daily_prices["4. close"]),
+            "volume": int(daily_prices["5. volume"])
+        }
+        rows.append(row)
+
+    return rows
+
+def write_to_csv(rows, csv_filepath):
+    # rows should be a list of dictionaries
+    # csv_filepath should be a string filepath pointing to where the data should be written
+
+    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+    with open(csv_filepath, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader() # uses fieldnames set above
+        for row in rows:
+            writer.writerow(row)
+
+    return True
+
 def to_usd(my_price):
-    return "${0:,.2f}".format(my_price)
+    # utility function to convert float or integer to usd-formatted string (for printing)
+    return "${0:,.2f}".format(my_price) #> $12,000.71
 
-# INFO INPUTS  
+if __name__ == "__main__":
 
-while True:
-    SYMBOL = input("Please input a stock symbol:")
-    if SYMBOL == "MIKE":
-        print("Hi, Professor Rossetti! Python rules! :)")
-    elif SYMBOL.isalpha() and len(SYMBOL) > 2 and len(SYMBOL) < 6 :
-        break
+    time_now = datetime.datetime.now() #> datetime.datetime(2019, 3, 3, 14, 44, 57, 139564)
+
+    #
+    # INFO INPUTS
+    #
+
+    while True:
+        symbol = input("Please input a symbol:")
+        if symbol.isalpha() and len(symbol) > 2 and len(symbol) < 6 :
+            break
+        else:
+            print("Please enter a stock symbol that is 3 to 5 characters, only letters A-Z.")
+
+    parsed_response = get_response(symbol)
+
+    last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+    rows = transform_response(parsed_response)
+
+    latest_close = rows[0]["close"]
+    high_prices = [row["high"] for row in rows] # list comprehension for mapping purposes!
+    low_prices = [row["low"] for row in rows] # list comprehension for mapping purposes!
+    recent_high = max(high_prices)
+    recent_low = min(low_prices)
+
+    #
+    # INFO OUTPUTS
+    #
+
+    # WRITE PRICES TO CSV FILE
+
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+    write_to_csv(rows, csv_filepath)
+
+    # DISPLAY RESULTS
+
+    formatted_time_now = time_now.strftime("%Y-%m-%d %H:%M:%S") #> '2019-03-03 14:45:27'
+
+    formatted_csv_filepath = csv_filepath.split("../")[1] #> data/prices.csv
+
+    print("-------------------------")
+    print(f"SYMBOL: {symbol}")
+    print("-------------------------")
+    print(f"REQUEST AT: {formatted_time_now}")
+    print(f"REFRESH DATE: {last_refreshed}")
+    print("-------------------------")
+    print(f"RECENT HIGH:  {to_usd(recent_high)}")
+    print(f"LATEST CLOSE: {to_usd(latest_close)}")
+    print(f"RECENT LOW:   {to_usd(recent_low)}")
+    print("-------------------------")
+
+    a = float(latest_close)
+    b = float(recent_high)
+    c = float (recent_low)
+    d = c*1.20
+
+    if a <= d: 
+        print("RECOMMENDATION: BUY!")
+        print("REASON: THE STOCK HAS SUFFICIENT GROWTH POTENTIAL TO SUGGEST INVESTING.")
     else:
-        print("Please enter a stock symbol that is 3 to 5 characters, only letters A-Z.")
+        print("RECOMMENDATION: DON'T BUY!")
+        print("REASON: THE STOCK DOES NOT HAVE SUFFICIENT GROWTH POTENTIAL TO SUGGEST INVESTING.")
 
-SEND_ADDRESS = input("Please input your email:")
-
-print("REQUESTING SOME DATA FROM THE INTERNET...")
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
-
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={SYMBOL}&apikey={API_KEY}"
-
-response = requests.get(request_url)
-
-parsed_response = json.loads(response.text)
-
-#if error message occurs
-
-if "Error Message" in response.text:
-    print("Oops, couldn't find that symbol. Please run the program again!")
-    exit()
-
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
-tsd = parsed_response["Time Series (Daily)"]
-
-dates = list(tsd.keys()) #TODO: sort to ensure latest day is first
-
-latest_day = dates[0] #"2020-02-19"
-
-latest_close = tsd[latest_day]["4. close"]
-
-#get high price from each day
-high_prices = []
-low_prices = []
-
-for date in dates: 
-    high_price = tsd[date]["2. high"]
-    low_price = tsd[date]["3. low"]
-    high_prices.append(float(high_price))
-    low_prices.append(float(low_price))
-
-#maximum of all high prices
-recent_high = max(high_prices)
-recent_low = min(low_prices)
-
-# INFO OUTPUTS
-
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-
-csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader() # uses fieldnames set above
-    for date in dates:
-         daily_prices = tsd[date]
-         writer.writerow({
-             "timestamp": date,
-             "open": daily_prices["1. open"],
-             "high": daily_prices["2. high"],
-             "low": daily_prices["3. low"],
-             "close": daily_prices["4. close"], 
-             "volume": daily_prices["5. volume"]
-         })
-
-import datetime
-now = datetime.datetime.now()
-
-print("-------------------------")
-print(f"SELECTED SYMBOL: {SYMBOL}")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT:")
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
-print("-------------------------")
-print(f"LATEST DAY: {last_refreshed}")
-print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print(f"RECENT LOW: {to_usd(float(recent_low))}")
-print("-------------------------")
-
-latest_close_var = float(latest_close)
-recent_high_var = float(recent_high)
-recent_low_var = float (recent_low)
-determinant = recent_low_var*1.2 
-
-if latest_close_var <= determinant: 
-    print("RECOMMENDATION: BUY!")
-    print("REASON: THE STOCK HAS SUFFICIENT GROWTH POTENTIAL TO SUGGEST INVESTING.")
-else:
-    print("RECOMMENDATION: DON'T BUY!")
-    print("REASON: THE STOCK DOES NOT HAVE SUFFICIENT GROWTH POTENTIAL TO SUGGEST INVESTING.")
-
-print("-------------------------")
-print(f"WRITING DATA TO CSV: {csv_file_path}...")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
-
-#---------------------------------------------------------------------------------------------------------
-
-if recent_high_var >= latest_close_var * 1.10:
-    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
-    MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
-    client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
-    print("CLIENT:", type(client))
-    subject = f"PRICE MOVEMENT ALERT: {SYMBOL} UP MORE THAN 10%"
-    html_content = f"Hey there! We are sending this message to inform you that {SYMBOL}, the stock you are tracking, has recently surpassed its latest closing price by more than 10%. This is a stock to watch -- happy investing!"
-    print("HTML:", html_content)
-    message = Mail(from_email=MY_ADDRESS, to_emails=SEND_ADDRESS, subject=subject, html_content=html_content)
-    try:
-        response = client.send(message)
-
-        print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
-        print(response.status_code) #> 202 indicates SUCCESS
-        print(response.body)
-        print(response.headers)
-    except Exception as e:
-        print("OOPS", e.message)
-else:
-    exit()
+    print(f"WRITING DATA TO CSV: {formatted_csv_filepath}")
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
