0ef9a703f2301e6cb6d48832f9975ba864a33b40
mr1451 <mr1451@georgetown.edu>
2020-04-15 20:32:06 -0400

    Revise app code with new recommendation function.

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,30 +1,41 @@
 # app/robo_advisor.py
 
+# import my packages
+import requests
 import csv
-import datetime
 import json
 import os
 
+import datetime
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 from dotenv import load_dotenv
-import requests
 
-load_dotenv() #> loads contents of the .env file into the script's environment
+load_dotenv()
 
-API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY", "demo") # default to using the "demo" key if an Env Var is not supplied
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
 
-def get_response(symbol):
+## DEFINE FUNCTIONS
+
+def to_usd(amount):
+    return "${0:,.2f}".format(amount)
+
+def compile_url(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
     response = requests.get(request_url)
+    #if symbol or API key is wrong
+    if "Error Message" in response.text:
+        print("We're sorry, but the stock ticker symbol you input does not exist per our records. Please try a different one!")
+        exit()
     parsed_response = json.loads(response.text)
     return parsed_response
 
 def transform_response(parsed_response):
-    # parsed_response should be a dictionary representing the original JSON response
-    # it should have keys: "Meta Data" and "Time Series Daily"
     tsd = parsed_response["Time Series (Daily)"]
-
     rows = []
-    for date, daily_prices in tsd.items(): # see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/dictionaries.md
+    for date, daily_prices in tsd.items():
         row = {
             "timestamp": date,
             "open": float(daily_prices["1. open"]),
@@ -34,94 +45,118 @@ def transform_response(parsed_response):
             "volume": int(daily_prices["5. volume"])
         }
         rows.append(row)
-
     return rows
 
 def write_to_csv(rows, csv_filepath):
-    # rows should be a list of dictionaries
-    # csv_filepath should be a string filepath pointing to where the data should be written
-
     csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-
-    with open(csv_filepath, "w") as csv_file:
+    with open(csv_filepath, "w", newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
         writer.writeheader() # uses fieldnames set above
         for row in rows:
             writer.writerow(row)
-
     return True
 
-def to_usd(my_price):
-    # utility function to convert float or integer to usd-formatted string (for printing)
-    return "${0:,.2f}".format(my_price) #> $12,000.71
+def recommendation(latest_close, recent_low):
+    calculation = (float(latest_close) - float(recent_low))/float(recent_low)
+    return calculation
 
 if __name__ == "__main__":
 
-    time_now = datetime.datetime.now() #> datetime.datetime(2019, 3, 3, 14, 44, 57, 139564)
+    #INFO CAPTURE
+    SYMBOL = input("Please input a valid stock ticker: ") #the resulting value is a string
 
-    #
-    # INFO INPUTS
-    #
+    #figure out the length of the input for data validation
+    if len(SYMBOL) > 4 or len(SYMBOL) < 3:
+        print("Your stock ticker symbol must be between 3-4 characters!")
+        exit()
 
-    while True:
-        symbol = input("Please input a symbol:")
-        if symbol.isalpha() and len(symbol) > 2 and len(symbol) < 6 :
-            break
-        else:
-            print("Please enter a stock symbol that is 3 to 5 characters, only letters A-Z.")
+    #figure out if the variable is an integer for error checking
+    has_errors = False
 
-    parsed_response = get_response(symbol)
+    try:
+        int_variable = int(SYMBOL)
+        has_errors = True
+    except:
+        pass
+    if has_errors == True:
+        print("The stock ticker symbol must be all letters. Please try again!")
+        exit()
 
+    # I USE A FUNCTION HERE
+    parsed_response = compile_url(SYMBOL)
+
+    # how to get latest day, latest close, recent high and low
     last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+    # I USE ANOTHER FUNCTION HERE
     rows = transform_response(parsed_response)
 
     latest_close = rows[0]["close"]
     high_prices = [row["high"] for row in rows] # list comprehension for mapping purposes!
     low_prices = [row["low"] for row in rows] # list comprehension for mapping purposes!
+
+
+    #get the high and low price from each day using min and max
     recent_high = max(high_prices)
     recent_low = min(low_prices)
 
-    #
-    # INFO OUTPUTS
-    #
-
-    # WRITE PRICES TO CSV FILE
+    #best way to get a CSV file path
 
     csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-
     write_to_csv(rows, csv_filepath)
 
-    # DISPLAY RESULTS
-
-    formatted_time_now = time_now.strftime("%Y-%m-%d %H:%M:%S") #> '2019-03-03 14:45:27'
-
-    formatted_csv_filepath = csv_filepath.split("../")[1] #> data/prices.csv
-
+    #info output
     print("-------------------------")
-    print(f"SYMBOL: {symbol}")
+    print("SELECTED SYMBOL: " + SYMBOL)
     print("-------------------------")
-    print(f"REQUEST AT: {formatted_time_now}")
-    print(f"REFRESH DATE: {last_refreshed}")
+    print("REQUESTING STOCK MARKET DATA...")
+    #datetime function
+    today = datetime.datetime.today()
+    print("REQUEST AT: " + today.strftime("%Y-%m-%d %I:%M %p"))
+    #continue to print the information from the parsed response
     print("-------------------------")
-    print(f"RECENT HIGH:  {to_usd(recent_high)}")
-    print(f"LATEST CLOSE: {to_usd(latest_close)}")
-    print(f"RECENT LOW:   {to_usd(recent_low)}")
+    print(f"LATEST DAY: {last_refreshed}")
+    print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+    print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+    print(f"RECENT LOW: {to_usd(float(recent_low))}")
     print("-------------------------")
 
-    a = float(latest_close)
-    b = float(recent_high)
-    c = float (recent_low)
-    d = c*1.20
+    benchmark = .10
+    email_trigger = 0.20
 
-    if a <= d: 
+    if(recommendation(latest_close, recent_low) < benchmark):
         print("RECOMMENDATION: BUY!")
         print("REASON: THE STOCK HAS SUFFICIENT GROWTH POTENTIAL TO SUGGEST INVESTING.")
+        print("THIS STOCK'S LATEST CLOSING PRICE IS BELOW 10% OF ITS RECENT LOW.")
     else:
-        print("RECOMMENDATION: DON'T BUY!")
+        print("RECOMMENDATION: DO NOT BUY!")
         print("REASON: THE STOCK DOES NOT HAVE SUFFICIENT GROWTH POTENTIAL TO SUGGEST INVESTING.")
-
-    print(f"WRITING DATA TO CSV: {formatted_csv_filepath}")
+        print("THIS STOCK'S LATEST CLOSING PRICE IS ABOVE 10% OF ITS RECENT LOW.")
+    print("-------------------------")
+    print(f"WRITING DATA TO CSV: {csv_filepath}")
     print("-------------------------")
     print("HAPPY INVESTING!")
     print("-------------------------")
+
+#---------------------------------------------------------------------------------------------------------
+    
+#if recent_high_var >= latest_close_var * 1.10:
+#    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+#    MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+#    client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+#    print("CLIENT:", type(client))
+#    subject = f"PRICE MOVEMENT ALERT: {SYMBOL} UP MORE THAN 10%"
+#    html_content = f"Hey there! We are sending this message to inform you that {SYMBOL}, the stock you are tracking, has recently surpassed its latest closing price by more than 10%. This is a stock to watch -- happy investing!"
+#    print("HTML:", html_content)
+#    message = Mail(from_email=MY_ADDRESS, to_emails=SEND_ADDRESS, subject=subject, html_content=html_content)
+#    try:
+#        response = client.send(message)
+#
+#        print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+#        print(response.status_code) #> 202 indicates SUCCESS
+#        print(response.body)
+#        print(response.headers)
+#    except Exception as e:
+#        print("OOPS", e.message)
+#else:
+#    exit()
--- robo_advisor_test.py
+++ robo_advisor_test.py
@@ -1,17 +1,16 @@
-#robo_advisor_test.py
-
 import os
-import pytest # for pytest.raises (see: https://docs.pytest.org/en/latest/assert.html)
+import pytest
 
-from app.robo_advisor import get_response, transform_response, write_to_csv, to_usd, human_friendly_timestamp
+from app.robo_advisor import compile_url, transform_response, write_to_csv, to_usd, calculations
 
 CI_ENV = os.environ.get("CI") == "true" # expect default environment variable setting of "CI=true" on Travis CI, see: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
 
 @pytest.mark.skipif(CI_ENV==True, reason="to avoid configuring credentials on, and issuing requests from, the CI server")
-def test_get_response():
+
+def test_compile_url():
     symbol = "NFLX"
 
-    parsed_response = get_response(symbol)
+    parsed_response = compile_url(symbol)
 
     assert isinstance(parsed_response, dict)
     assert "Meta Data" in parsed_response.keys()
@@ -60,61 +59,34 @@ def test_transform_response():
 
     assert transform_response(parsed_response) == transformed_response
 
-#def test_write_to_csv():
-#
-#    # SETUP
-#
-#    example_rows = [
-#        {"timestamp": "2019-06-08", "open": "101.0924", "high": "101.9500", "low": "100.5400", "close": "101.6300", "volume": "22165128"},
-#        {"timestamp": "2019-06-07", "open": "102.6500", "high": "102.6900", "low": "100.3800", "close": "100.8800", "volume": "28232197"},
-#        {"timestamp": "2019-06-06", "open": "102.4800", "high": "102.6000", "low": "101.9000", "close": "102.4900", "volume": "21122917"},
-#        {"timestamp": "2019-06-05", "open": "102.0000", "high": "102.3300", "low": "101.5300", "close": "102.1900", "volume": "23514402"},
-#        {"timestamp": "2019-06-04", "open": "101.2600", "high": "101.8600", "low": "100.8510", "close": "101.6700", "volume": "27281623"},
-#        {"timestamp": "2019-06-01", "open": '99.2798',  "high": "100.8600", "low": "99.1700",  "close": "100.7900", "volume": "28655624"}
-#    ]
-#
-#    csv_filepath = os.path.join(os.path.dirname(__file__), "example_reports", "temp_prices.csv")
-#
-#    if os.path.isfile(csv_filepath):
-#        os.remove(csv_filepath)
-#
-#    assert os.path.isfile(csv_filepath) == False # just making sure the test was setup properly
-#
-#    # INVOCATION
-#
-#    result = write_to_csv(example_rows, csv_filepath)
-#
-#    # EXPECTATIONS
-#
-#    assert result == True
-#    assert os.path.isfile(csv_filepath) == True
-#    # TODO: consider also testing the file contents!
-#
+def test_write_to_csv():
+
+    # SETUP
+
+    example_rows = [
+        {"timestamp": "2019-06-08", "open": "101.0924", "high": "101.9500", "low": "100.5400", "close": "101.6300", "volume": "22165128"},
+        {"timestamp": "2019-06-07", "open": "102.6500", "high": "102.6900", "low": "100.3800", "close": "100.8800", "volume": "28232197"},
+        {"timestamp": "2019-06-06", "open": "102.4800", "high": "102.6000", "low": "101.9000", "close": "102.4900", "volume": "21122917"},
+        {"timestamp": "2019-06-05", "open": "102.0000", "high": "102.3300", "low": "101.5300", "close": "102.1900", "volume": "23514402"},
+        {"timestamp": "2019-06-04", "open": "101.2600", "high": "101.8600", "low": "100.8510", "close": "101.6700", "volume": "27281623"},
+        {"timestamp": "2019-06-01", "open": '99.2798',  "high": "100.8600", "low": "99.1700",  "close": "100.7900", "volume": "28655624"}
+    ]
+
+    csv_filepath = os.path.join(os.path.dirname(__file__), "example_reports", "temp_prices.csv")
+
+    if os.path.isfile(csv_filepath):
+        os.remove(csv_filepath)
+
+    assert os.path.isfile(csv_filepath) == False # just making sure the test was setup properly
+
+    # INVOCATION
+
+    result = write_to_csv(example_rows, csv_filepath)
+
+    # EXPECTATIONS
+
+    assert result == True
+    assert os.path.isfile(csv_filepath) == True
 
 def test_to_usd():
-    """
-    Tests that the to_usd function correctly reformats price values in US dollars.
-    """
-    # it should apply USD formatting
-    assert to_usd(4.50) == "$4.50"
-
-    # it should display two decimal places
-    assert to_usd(4.5) == "$4.50"
-
-    # it should round to two places
-    assert to_usd(4.55555) == "$4.56"
-
-    # it should display thousands separators
-    assert to_usd(1234567890.5555555) == "$1,234,567,890.56"
-
-def test_human_friendly_timestamp():
-    """
-    Tests that the human_friendly_timestamp successfully converts the datetime object into an easy-to-read timestamp string.
-    """
-    # it should display the time in an human-friendly format (round to the nearest minute, add AM or PM timestamp)
-    # update by the second
-    assert human_friendly_timestamp != "2020-04-14 05:16 PM"
-
-def test_float_conversion():
-    assert float("102.6500") == 102.65
-    assert float("101.0924") == 101.0924 # interesting but helpful that the full value is retained
+    assert to_usd(123456.8) == "$123,456.80"
